e1933868758c02391cf6bddc6761b67670d056bb
Michail Zyusko <67417124+MichailZyusko@users.noreply.github.com>
2022-09-07 08:52:55 +0300

    Update pm2 config file (#3)

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -1,18 +1,29 @@
 module.exports = {
-  apps : [{
+  apps: [{
+    // General
     name : "benefit",
     script: "./dist/main.js",
-    exp_backoff_restart_delay: 3000,
-    max_memory_restart: '450M',
-    max_restarts: 10,
-    watch: true,
+
+    // Advanced features
     instances: 2, 
     exec_mode: "cluster",
+      watch: true,
+
     env_production: {
       NODE_ENV: "production"
     },
     env_development: {
       NODE_ENV: "development"
-    }
+    },
+
+    // Log files
+    error_log: './logs/error.log',
+    out_file: './logs/output.log',
+
+    // Control flow
+    max_restarts: 10,
+        exp_backoff_restart_delay: 3000,
+    max_memory_restart: '450M',
+
   }]
 }
